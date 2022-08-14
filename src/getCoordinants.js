88a75f53b312c0a7e5d33121e993c302067604ce
BParadowski <bartoszparadowski01@gmail.com>
2022-08-14 17:04:03 +0200

    fix http(s) request

--- src/getCoordinants.js
+++ src/getCoordinants.js
@@ -1,7 +1,7 @@
 export default async function getCoordinants(place) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${place}&appid=c163d488922d9e2b4e18ae84875b853c`,
+      `https://api.openweathermap.org/geo/1.0/direct?q=${place}&appid=c163d488922d9e2b4e18ae84875b853c`,
       { mode: "cors" }
     );
     const dataArray = await response.json();
